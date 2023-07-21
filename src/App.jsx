af0d764fc30469dc57d87cd98da1b2f3e4f24e6a
memeoplast <dafough@gmail.com>
2023-07-21 14:56:37 +0200

    Adeed Carousel.jsx and .css for Carousel component. Added react multi carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from "./Components/Navbar/Navbar.jsx"
 import Body from "./Components/Body/Body.jsx"
 import Footer from "./Components/Footer/Footer.jsx"
+import ResponsiveCarousel from "./Components/Carousel/Carousel.jsx"
 import { createContext, useState } from "react";
 import { ThemeContext } from "./Components/ThemeContext"
 
@@ -20,6 +21,7 @@ const toggleTheme = () => {
       <div className="App" id={isDark}>
         <Navbar />
         <Body />
+        <ResponsiveCarousel />
         <Footer />
       </div>
       </ThemeContext.Provider>
---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,46 @@
+import Carousel from "react-multi-carousel";
+import "../Carousel/Carousel.css";
+import "react-multi-carousel/lib/styles.css";
+
+
+
+const ResponsiveCarousel = () => {
+    const responsive = {
+        LargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 6
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        },
+    };
+
+    return (
+        <div className="block__content">
+            <h1 className="heading__primary">Products</h1>
+            <Carousel
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                responsive={responsive}
+                >
+                <div className="carousel__item">Item 1</div>
+                <div className="carousel__item">Item 2</div>
+                <div className="carousel__item">Item 3</div>
+                <div className="carousel__item">Item 4</div>
+            </Carousel>
+        </div>
+    )
+
+};
+
+export default ResponsiveCarousel;
