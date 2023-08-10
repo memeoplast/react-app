6dbf00596bda46af42d1b413031b4c7165df79a5
Nico G <91379265+memeoplast@users.noreply.github.com>
2023-08-10 15:33:33 +0200

    changed images path for carousel data added subfolder public/images

--- src/Components/Carousel/data.js
+++ src/Components/Carousel/data.js
@@ -20,31 +20,31 @@ export const responsive = {
   export const productData = [
     {
       id: 1,
-      imageurl:"../../public/IT-Consulting.jpg",
+      imageurl:"/images/IT-Consulting.jpg",
       name: "IT Consultation and Support",
       description: "Get expert advice and reliable IT support for your business needs, from network setup and cybersecurity to software implementation and troubleshooting.",
     },
     {
       id: 2,
-      imageurl:"../../public/Custom-Software.jpg",
+      imageurl:"/images/Custom-Software.jpg",
       name: "Custom Software Development",
       description: "Enhance your operations with tailor-made software solutions designed to streamline processes, improve efficiency, and meet your specific business requirements.",
     },
     {
       id: 3,
-      imageurl:"../../public/Training.jpg",
+      imageurl:"/images/Training.jpg",
       name: "Corporate Training Programs",
       description: "Empower your workforce with our specialized corporate training programs, covering leadership development, team-building, communication skills, and more.",
     },
     {
       id: 4,
-      imageurl:"../../public/Analytics.jpg",
+      imageurl:"/images/Analytics.jpg",
       name: "Business Analytics and Insights",
       description: "Make informed decisions and stay ahead of the competition by leveraging our data analysis and insights services, providing valuable information for strategic planning.",
     },
     {
       id: 5,
-      imageurl:"../../public/Sustainability.jpg",
+      imageurl:"/images/Sustainability.jpg",
       name: "Sustainability Consulting",
       description: "Embrace eco-friendly practices and enhance your corporate social responsibility with our sustainability consulting, helping you create a positive impact on the environment and society.",
     },
